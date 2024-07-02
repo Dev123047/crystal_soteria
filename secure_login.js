225427c7a43ea9a6e74ad62161f31430561e6bdc
Bhavy Garg <66347950+Dev12310@users.noreply.github.com>
2024-07-02 21:46:35 +0530

    Update secure_login.js

--- secure_login.js
+++ secure_login.js
@@ -1,7 +1,8 @@
 //feels like thats a LOT of vars for a "secure" system lol 
 var hashed = {
     'localhost_test_user1' : 'vqntEjWGio2yhR3wN/0QmF8xEnPBo4jya4wS5c39n8/pyrx0Z1VlRDAVe1vw0MQhw35EwartPhjgIodpNEIIVA==',
-    'host1_test_user2' : 'NlNTUeuKMJofSOTJv3C/ZSxQWHMzjNRSud/0ydQOSbLCzxqDdj1w4WApn70D9eABsymx5a3DOQdNDwBczWOy3w=='
+    'host1_test_user2' : 'NlNTUeuKMJofSOTJv3C/ZSxQWHMzjNRSud/0ydQOSbLCzxqDdj1w4WApn70D9eABsymx5a3DOQdNDwBczWOy3w==',
+    'localhost_secret' : 'M5JjKkM2BMq2ALO8PUeMSg4lZqDvYpeYqy/+nazz60QHQsiISYLCbQMhAKRwHUa6tCaM0hFOljhmVUSC2dXBCw=='
 }
 const salt = "oOcCVsMWUqBD5W38RGPl8g="
 var current_data = '';
